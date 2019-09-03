01cf3e8de369aa98fdeec60af85b08647bc49ef6
congyaozhu <1965326398@qq.com>
2019-09-03 23:34:44 +0800

    添加 mysql 依赖 MySQL数据库连接信息 初始化 SnowFlakeIdWorker 初始化 UUIDUtils 增加SnowFlakeIdWorker测试代码

--- src/test/java/com/personalstudy/goodstudy/Test1.java
+++ src/test/java/com/personalstudy/goodstudy/Test1.java
@@ -1,5 +1,7 @@
 package com.personalstudy.goodstudy;
 
+import com.personalstudy.goodstudy.util.SnowflakeIdWorker;
+
 /**
  * @author : <a href="mailto:congyaozhu@ebnew.com">congyaozhu</a>
  * @Date : Created in  14:39 2019-08-15
@@ -7,6 +9,11 @@ package com.personalstudy.goodstudy;
  */
 public class Test1 {
     public static void main(String[] args) {
+        long num = SnowflakeIdWorker.init().nextId();
+        System.out.println(num);
+    }
+
+    public static void method1(){
         Integer i01=59;
         int i02=59;
         Integer i03=Integer.valueOf(59);

8ac7caa428c1a044ecb6ffd35be5394b035678e5
congyaozhu <congyaozhu@ebnew.com>
2019-09-17 17:14:41 +0800

    配置tomcat 请求不合法问题，支持url包含{}[]|

---
+++ family-bucket/src/main/java/com/personalstudy/goodstudy/util/PortalTomcatWebServerCustomizer.java
@@ -0,0 +1,13 @@
+package com.personalstudy.goodstudy.util;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortalTomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setAttribute("relaxedQueryChars", "{}[]|"));
+    }
+}

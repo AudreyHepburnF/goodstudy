9fb9f347f0f9b0f7553c3838947131f50f6d2230
congyaozhu <congyaozhu@ebnew.com>
2019-12-26 16:52:52 +0800

    删除链表的倒数第N个节点

--- interview/src/main/java/com/goodstudy/interview/dataStructure/Solution.java
+++ interview/src/main/java/com/goodstudy/interview/dataStructure/Solution.java
@@ -3,8 +3,7 @@ package com.goodstudy.interview.dataStructure;
 /**
  * @author : <a href="mailto:congyaozhu@ebnew.com">congyaozhu</a>
  * @Date : Created in  15:05 2019-06-20
- * @Description :在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
- * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * @Description :
  */
 public class Solution {
 
@@ -17,6 +16,15 @@ public class Solution {
         }
     }
 
+    /**
+     * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+     *  * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     *  * 解题思路： 循环数组长度，行从0开始，列从行最后一位开始
+     *  *          判断给定的值，是否大于当前行的最大值，如果大于，直接行++，否则列--
+     * @param target
+     * @param array
+     * @return
+     */
     public static boolean Find(int target, int [][] array) {
         int row = 0 ;
         int col = array[0].length-1;
@@ -30,5 +38,39 @@ public class Solution {
             }
         }
         return false;
+    }
+
+    /**
+     * 删除链表的倒数第N个节点
+     *      给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+     *
+     * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // first先移动n次，与second保留n个间隔
+        for (int i = 1; i <= n+1; i++) {
+            first = first.next;
+        }
+
+        // 判断first是否是尾节点
+        while (first != null){
+            first = first.next;
+            second = second.next;
+        }
+
+        // 将second的next节点与second.next.next节点关联
+        second.next = second.next.next;
+        return dummy.next;
+
     }
 }

92c85e53f237e47ba434afd58280eab1b9f33262
congyaozhu <congyaozhu@ebnew.com>
2020-06-02 07:06:59 +0800

    jvm案例代码提交

---
+++ tuling-jvm/src/main/java/com/personalstudy/tuling/jvm/DealLockTest.java
@@ -0,0 +1,42 @@
+package com.personalstudy.tuling.jvm;
+
+/**
+ * @author congyaozhu
+ * @date 2020-05-08 22:10
+ * @description
+ */
+public class DealLockTest {
+
+    private static Object lock1 = new Object();
+    private static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (lock1) {
+                try {
+                    System.out.println("thread1 begin");
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                }
+                synchronized (lock2) {
+                    System.out.println("thread1 end");
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (lock2) {
+                try {
+                    System.out.println("thread2 begin");
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                }
+                synchronized (lock1) {
+                    System.out.println("thread2 end");
+                }
+            }
+        }).start();
+
+        System.out.println("main thread end");
+    }
+}

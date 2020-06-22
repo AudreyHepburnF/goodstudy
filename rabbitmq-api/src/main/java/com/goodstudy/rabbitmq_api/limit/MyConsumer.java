7efd4f3fd87d397b128d41963171b165d3fe27f0
congyaozhu <congyaozhu@ebnew.com>
2020-06-22 23:39:19 +0800

    RabbitMQ 消费端限流

---
+++ rabbitmq-api/src/main/java/com/goodstudy/rabbitmq_api/limit/MyConsumer.java
@@ -0,0 +1,37 @@
+package com.goodstudy.rabbitmq_api.limit;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+/**
+ * @author congyaozhu
+ * @date 2020-06-22 23:22
+ * @description
+ */
+public class MyConsumer extends DefaultConsumer {
+
+    private Channel channel;
+
+    public MyConsumer(Channel channel) {
+        super(channel);
+        this.channel = channel;
+    }
+
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        System.err.println("-----------consume message----------");
+        System.err.println("consumerTag: " + consumerTag);
+        System.err.println("envelope: " + envelope);
+        System.err.println("properties: " + properties);
+        System.err.println("body: " + new String(body));
+
+        //确认消息的方法，回调成功以后再执行下一条，表示这条消息我已经处理完了，你可以给我下一条了。false表示不批量签收
+        channel.basicAck(envelope.getDeliveryTag(),false);
+
+    }
+}
+
---
+++ rabbitmq-api/src/main/java/com/goodstudy/rabbitmq_api/limit/Producer.java
@@ -0,0 +1,32 @@
+package com.goodstudy.rabbitmq_api.limit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author congyaozhu
+ * @date 2020-06-22 23:21
+ * @description 自定义消费端限流  生产者代码
+ */
+public class Producer {
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String exchange = "test_qos_exchange";
+        String routingKey = "qos.save";
+
+        String msg = "Hello RabbitMQ QOS Message";
+
+        for(int i =0; i<5; i ++){
+            channel.basicPublish(exchange, routingKey, true, null, msg.getBytes());
+        }
+    }
+}
+

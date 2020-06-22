993d04e2d523c4bf24d12c3b95a3381a34ed708c
congyaozhu <congyaozhu@ebnew.com>
2020-06-22 23:26:52 +0800

    RabbitMQ 自定义消费者

---
+++ rabbitmq-api/src/main/java/com/goodstudy/rabbitmq_api/consumer/Producer.java
@@ -0,0 +1,35 @@
+package com.goodstudy.rabbitmq_api.consumer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author congyaozhu
+ * @date 2020-06-22 23:17
+ * @description 自定义消费者  生产端代码
+ */
+public class Producer {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String exchange = "test_consumer_exchange";
+        String routingKey = "consumer.save";
+
+        String msg = "Hello RabbitMQ Consumer Message";
+
+        for(int i =0; i<5; i ++){
+            channel.basicPublish(exchange, routingKey, true, null, msg.getBytes());
+        }
+    }
+}
